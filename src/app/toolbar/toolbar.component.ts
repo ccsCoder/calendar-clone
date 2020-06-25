f1b36f798eef49c566405ea02c8f65cb1813704b
Nishant Gupta <nishantgupta@MacBook-Pro.local>
2020-06-25 17:56:47 +0530

    Date-picker message change on selection

--- src/app/toolbar/toolbar.component.ts
+++ src/app/toolbar/toolbar.component.ts
@@ -22,6 +22,8 @@ export class ToolbarComponent implements OnInit {
 
   onDateSelected($event) {
     console.log($event.value);
+    this.month = moment($event.value).format('MMMM').toString();
+    this.year = moment($event.value).format('YYYY').toString();
     this.calendarActionsService.dateSelectionOccured($event.value);
   }
 
